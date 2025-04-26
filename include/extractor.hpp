0d7d3aff2cb4d5505838cea44be46ab7e19ed150
Ajay Wakodikar <ajaywakodikarsocial@gmail.com>
2025-04-26 23:04:20 +0530

    Extractor: Simulates data fetching

---
+++ include/extractor.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+// Extractor class: simulates data fetching
+class Extractor {
+    public:
+    // Returns extracted data (dummy here)
+    std::string extract();
+};
---
+++ source/extractor.cpp
@@ -0,0 +1,7 @@
+#include "extractor.hpp"
+
+// Simulate extracting one data item
+std::string Extractor::extract() {
+    // In real case: fetch from DB, file, API
+    return "data item";
+}
