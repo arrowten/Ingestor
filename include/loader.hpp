41b46820b140140079205f9af2e47eaba12eebb1
Ajay Wakodikar <ajaywakodikarsocial@gmail.com>
2025-04-26 23:06:56 +0530

    Loader: Loads or stores transformed data

---
+++ include/loader.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+// Loader class: loads or stores transformed data
+class Loader {
+    public:
+    // Load/output the processed data
+    void load(const std::string& output);
+};
---
+++ source/loader.cpp
@@ -0,0 +1,7 @@
+#include "loader.hpp"
+#include <iostream>
+
+// Print the loaded data (simulate storing)
+void Loader::load(const std::string& output) {
+    std::cout << "Loaded: " << output << std::endl;
+}
